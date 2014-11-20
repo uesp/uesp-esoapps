610789105c9c8138b22d91b6ea3a31ac7e3e92bd
Dave Humphrey uespnet <Dave Humphrey uespnet@gmail.com>
2014-11-20 22:45:46 +0000

    add lua directory tree output

--- parseGlobals/EsoEnvironment.py
+++ parseGlobals/EsoEnvironment.py
@@ -3,6 +3,7 @@ import operator
 import sys
 import datetime
 import shutil
+from string import Template
 import EsoGlobals
 from EsoGlobals import CEsoGlobals
 import EsoLuaFile
@@ -12,6 +13,8 @@ from EsoFunctionInfo import CEsoFunctionInfo
 from EsoFunctionInfo import CEsoFunctionCallInfo
 import EsoFunctionDb
 from EsoFunctionDb import CEsoFunctionDb
+import EsoLuaTokenizer
+from EsoLuaTokenizer import Token
 
 
 class CEsoEnvironment:
@@ -22,6 +25,10 @@ class CEsoEnvironment:
         self.functions = CEsoFunctionInfo()
         self.functionCalls = CEsoFunctionCallInfo()
         self.functionDb = CEsoFunctionDb()
+        self.luaDirHeaderTemplate = Template(open('templates/esoluadir_header.txt', 'r').read())
+        self.luaDirFooterTemplate = Template(open('templates/esoluadir_footer.txt', 'r').read())
+        self.luaFileHeaderTemplate = Template(open('templates/esoluafile_header.txt', 'r').read())
+        self.luaFileFooterTemplate = Template(open('templates/esoluafile_footer.txt', 'r').read())
 
 
     def LoadGlobals(self, filename):
@@ -37,3 +44,224 @@ class CEsoEnvironment:
         self.functionDb = EsoFunctionDb.CreateDb(self.functions, self.functionCalls)
         self.functionDb.CreateFunctionValueMap(self.globalData)
         self.functionDb.MatchGlobals(self.globalData)
+
+
+    def CreateGlobalTemplateVars(self):
+        templateVars = { }
+        
+        templateVars["parseTime"] = self.globalData.parseTime
+        templateVars["parseDate"] = self.globalData.parseDate
+        templateVars["parseVersion"] = self.globalData.parseVersion
+        templateVars["creationDate"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        return templateVars
+
+    
+    def CreateLuaFileTemplateVars(self, luaFile):
+        templateVars = self.CreateGlobalTemplateVars()
+
+        templateVars["filename"] = luaFile.relFilename
+        templateVars["fullFilename"] = luaFile.fullFilename
+        templateVars["creationDate"] = luaFile.creationDate
+        templateVars["numTokens"] = str(len(luaFile.tokenizer.tokens))
+        templateVars["resourcePath"] = ""
+
+        return templateVars
+        
+
+    def CreateLuaFileHtmlContent(self, outFile, luaFile):
+        tokens = luaFile.tokenizer.tokensWithComments
+                
+        lastToken = EsoLuaTokenizer.CLuaToken()
+        lastToken.linePos = 1
+        lastToken.charPos = 0
+        lastToken.charIndex = 0
+
+        outFile.write("<div id='esolf_fileroot'>\n")
+        outFile.write("<table border='0' cellpadding='0' cellspacing='0'>\n")
+        outFile.write("<tbody>\n")
+        outFile.write("<tr>\n")
+
+        lines = luaFile.fileContents.split("\n")
+        outFile.write("<td class='esolf_linenumbers'>\n")
+        lineNumber = 1
+
+        for line in lines:
+            outFile.write("\t<div id='{0}' class='esolf_lineno'>{0}</div>".format(lineNumber))
+            lineNumber += 1
+
+        outFile.write("</td>\n")
+        outFile.write("<td class='esolf_codelines'>\n")
+        outFile.write("\t<div class='esolf_codeline'>")
+        lineNumber = 1
+
+        for token in tokens:
+            startIndex = lastToken.charIndex + len(lastToken.token)
+            endIndex = token.charIndex
+            deltaString = luaFile.fileContents[startIndex : endIndex]
+            #print "{0}: {1} - {2} = '{3}'".format(lineNumber, startIndex, endIndex, deltaString)
+            #print "{0}:{2} - {1}, delta='{3}'".format(lineNumber, token.token, token.linePos, deltaString)
+
+            if (len(deltaString) > 0):
+                deltaString = deltaString.replace("\t", "     ").replace(" ", "&nbsp;")
+                lines = deltaString.split("\n")
+                lineIndex = 0
+
+                for line in lines:
+                    if len(line) != 0:
+                        outFile.write("<code class='esolf_space'>{0}</code>".format(line))
+
+                    if (lineIndex + 1 < len(lines)):
+                        lineIndex += 1
+                        outFile.write("</div>\n")
+                        outFile.write("\t<div class='esolf_codeline'>")
+                        
+                lineNumber = token.linePos
+            elif (lineNumber != token.linePos):
+                outFile.write("</div>\n")
+                outFile.write("\t<div class='esolf_codeline'>")
+                lineNumber = token.linePos
+
+            tooltip = ""
+            
+            if (token.type == Token.name):
+                obj = self.globalData.GetGlobal(token.token)
+                
+                if obj is None:
+                    tooltip = ""
+                elif obj.type == "number" and obj.string != "":
+                    tooltip = "({0})\"{1}\"".format(obj.value, obj.string)
+                elif obj.type == "number":
+                    tooltip = obj.value
+                elif obj.type == "string":
+                    tooltip = obj.value
+                elif obj.type == "function":
+                    tooltip = "function:" + obj.value
+                elif obj.type == "userdata":
+                    tooltip = "userdata:" + obj.value
+                elif obj.type == "table":
+                    tooltip = "table:" + obj.value
+
+                if len(tooltip) > 100: tooltip = tooltip[:100] + "..."
+                tooltip = tooltip.replace(">", "&gt;").replace("<", "&lt;")
+
+            outputText = token.token.replace(">", "&gt;").replace("<", "&lt;")
+
+            if (tooltip == ""):
+                outFile.write("<code class='esolf_{0}'>{1}</code>".format(Token.toString(token.type), outputText))
+            else:
+                outFile.write("<code class='esolf_{0}' tooltip='{2}'>{1}</code>".format(Token.toString(token.type), outputText, tooltip))
+
+            lastToken = token
+
+        outFile.write("</div>\n")    
+        outFile.write("</td>\n")
+        outFile.write("</tr>\n")
+        outFile.write("</tbody>\n")
+        outFile.write("</table>\n")
+        outFile.write("</div>\n")
+        return True
+
+
+    def CopyLuaFileHtmlResources(self, outputPath):
+        shutil.copyfile("resources/esoluafile.css", os.path.join(outputPath, '') + "esoluafile.css")
+        return True
+
+
+    def CreateLuaFileHtml(self, luaFile, outputBasePath, copyResources = True):
+        outputBasePath = os.path.join(outputBasePath, "").replace("\\", "/")
+        outputFilename = outputBasePath + luaFile.relFilename
+        outputHtmlFilename = outputFilename  + ".html"
+        path = os.path.dirname(outputFilename)
+        if not os.path.exists(path): os.makedirs(path)
+
+        if copyResources:
+            self.CopyLuaFileHtmlResources(os.path.dirname(os.path.dirname(outputBasePath)))
+            
+        shutil.copyfile(luaFile.fullFilename, outputFilename)
+
+        templateVars = self.CreateLuaFileTemplateVars(luaFile)
+        templateVars["resourcePath"] = os.path.relpath(outputBasePath, outputFilename).replace("\\", "/")
+
+        with open(outputHtmlFilename, "w") as outFile:
+            outFile.write(self.luaFileHeaderTemplate.safe_substitute(templateVars))
+            self.CreateLuaFileHtmlContent(outFile, luaFile)
+            outFile.write(self.luaFileFooterTemplate.safe_substitute(templateVars))
+        
+        return True
+
+
+    def CreateLuaFilesHtml(self, outputBasePath):
+        hasOutputResources = False
+        print "Creating HTML versions of {0} Lua files...".format(len(self.luaFiles))
+
+        for luaFile in self.luaFiles:
+            self.CreateLuaFileHtml(luaFile, outputBasePath, not hasOutputResources)
+            hasOutputResources = True
+
+
+    def OutputLuaFilesDirTree(self, root, outputBasePath, parentPath):
+        outputDirs = ""
+        outputFiles = ""
+        if (parentPath != ""): outputDirs += "<li><a href='../luadir.html'>[dir] ..</a></li>\n"
+        sortedKeys = sorted(root.keys())
+
+        for key in sortedKeys:
+            entry = root[key]
+
+            if (type(entry) is dict):
+                link = "<a href='{0}/luadir.html'>[dir] {0}</a>".format(key)
+                outputDirs += "<li class='esold_dir'>{0}</li>\n".format(link)
+                self.OutputLuaFilesDirTree(entry, outputBasePath, parentPath + key + "/")
+            else:
+                link = "<a href='{0}.html'>{0}</a>".format(key)
+                outputFiles += "<li class='esold_file'>{0}</li>\n".format(link)
+
+        filename = os.path.join(outputBasePath, parentPath, '') + "luadir.html"
+        path = os.path.dirname(filename)
+        if not os.path.exists(path): os.makedirs(path)
+
+        templateVars = self.CreateGlobalTemplateVars()
+        templateVars["resourcePath"] = os.path.relpath(os.path.dirname(os.path.dirname(outputBasePath)), path).replace("\\", "/")
+        templateVars["luaPath"] = parentPath
+        
+        with open(filename, "w") as outFile:
+            outFile.write(self.luaDirHeaderTemplate.safe_substitute(templateVars))
+            outFile.write("<ul class='esold_list'>\n")
+            outFile.write(outputDirs)
+            outFile.write(outputFiles)
+            outFile.write("</ul>\n")
+            outFile.write(self.luaDirFooterTemplate.safe_substitute(templateVars))
+
+
+    def CreateLuaFilesDirTree(self, outputBasePath):
+        rootFiles = { }
+
+        print "Creating the Lua files directory tree to {0}...".format(outputBasePath)
+
+        outputBasePath = os.path.join(outputBasePath, "").replace("\\", "/")
+        if not os.path.exists(outputBasePath): os.makedirs(outputBasePath)
+
+                # Create the directory/file tree
+        for luaFile in self.luaFiles:
+            split = luaFile.relFilename.split("/")
+            paths = split[:-1]
+            filename = split[-1]
+            root = rootFiles
+
+            #print "{0} ==> '{1}' + {2}".format(luaFile.relFilename, paths, filename)
+
+            for path in paths:
+                if (not path in root): root[path] = { }
+                root = root[path]
+
+            root[filename] = luaFile
+
+        #print rootFiles
+        shutil.copyfile("resources/esoluadir.css", os.path.dirname(os.path.dirname(os.path.join(outputBasePath, ''))) + "/esoluadir.css")
+        self.OutputLuaFilesDirTree(rootFiles, outputBasePath, "")
+        
+
+        
+            
+
--- parseGlobals/EsoLuaFile.py
+++ parseGlobals/EsoLuaFile.py
@@ -15,10 +15,6 @@ from EsoLuaTokenizer import Token
 
 class CEsoLuaFile:
 
-    headerTemplate = Template(open('templates/esoluafile_header.txt', 'r').read())
-    footerTemplate = Template(open('templates/esoluafile_footer.txt', 'r').read())
-    
-
     def __init__(self):
         self.fullFilename = ""
         self.relFilename = ""
@@ -26,7 +22,6 @@ class CEsoLuaFile:
         self.creationDate = ""
         self.tokenizer = CLuaTokenizer()
         self.localObjects = { }
-        
         self.objects = { }
 
 
@@ -48,151 +43,7 @@ class CEsoLuaFile:
 
         self.fileContents = open(filename, "r").read()
         return self.Parse()
-
-
-    def CreateTemplateVars(self, globalData):
-        templateVars = { }
-
-        templateVars["filename"] = self.relFilename
-        templateVars["fullFilename"] = self.fullFilename
-        templateVars["creationDate"] = self.creationDate
-        templateVars["numTokens"] = str(len(self.tokenizer.tokens))
-        templateVars["parseTime"] = globalData.parseTime
-        templateVars["parseDate"] = globalData.parseDate
-        templateVars["parseVersion"] = globalData.parseVersion
-        templateVars["resourcePath"] = ""
-
-        return templateVars
-        
-
-    def CreateHtmlContent(self, outFile, globalData):
-        tokens = self.tokenizer.tokensWithComments
-                
-        lastToken = EsoLuaTokenizer.CLuaToken()
-        lastToken.linePos = 1
-        lastToken.charPos = 0
-        lastToken.charIndex = 0
-
-        outFile.write("<div id='esolf_fileroot'>\n")
-        outFile.write("<table border='0' cellpadding='0' cellspacing='0'>\n")
-        outFile.write("<tbody>\n")
-        outFile.write("<tr>\n")
-
-        lines = self.fileContents.split("\n")
-        outFile.write("<td class='esolf_linenumbers'>\n")
-        lineNumber = 1
-
-        for line in lines:
-            outFile.write("\t<div id='{0}' class='esolf_lineno'>{0}</div>".format(lineNumber))
-            lineNumber += 1
-
-        outFile.write("</td>\n")
-        outFile.write("<td class='esolf_codelines'>\n")
-        outFile.write("\t<div class='esolf_codeline'>")
-        lineNumber = 1
-
-        for token in tokens:
-            startIndex = lastToken.charIndex + len(lastToken.token)
-            endIndex = token.charIndex
-            deltaString = self.fileContents[startIndex : endIndex]
-            #print "{0}: {1} - {2} = '{3}'".format(lineNumber, startIndex, endIndex, deltaString)
-            #print "{0}:{2} - {1}, delta='{3}'".format(lineNumber, token.token, token.linePos, deltaString)
-
-            if (len(deltaString) > 0):
-                deltaString = deltaString.replace("\t", "     ").replace(" ", "&nbsp;")
-                lines = deltaString.split("\n")
-                lineIndex = 0
-
-                for line in lines:
-                    if len(line) != 0:
-                        outFile.write("<code class='esolf_space'>{0}</code>".format(line))
-
-                    if (lineIndex + 1 < len(lines)):
-                        lineIndex += 1
-                        outFile.write("</div>\n")
-                        outFile.write("\t<div class='esolf_codeline'>")
-                        
-                lineNumber = token.linePos
-            elif (lineNumber != token.linePos):
-                outFile.write("</div>\n")
-                outFile.write("\t<div class='esolf_codeline'>")
-                lineNumber = token.linePos
-
-            tooltip = ""
-            
-            if (token.type == Token.name):
-                obj = globalData.GetGlobal(token.token)
-                
-                if obj is None:
-                    tooltip = ""
-                elif obj.type == "number" and obj.string != "":
-                    tooltip = "({0})\"{1}\"".format(obj.value, obj.string)
-                elif obj.type == "number":
-                    tooltip = obj.value
-                elif obj.type == "string":
-                    tooltip = obj.value
-                elif obj.type == "function":
-                    tooltip = "function:" + obj.value
-                elif obj.type == "userdata":
-                    tooltip = "userdata:" + obj.value
-                elif obj.type == "table":
-                    tooltip = "table:" + obj.value
-
-                if len(tooltip) > 100: tooltip = tooltip[:100] + "..."
-                tooltip = tooltip.replace(">", "&gt;").replace("<", "&lt;")
-
-            outputText = token.token.replace(">", "&gt;").replace("<", "&lt;")
-
-            if (tooltip == ""):
-                outFile.write("<code class='esolf_{0}'>{1}</code>".format(Token.toString(token.type), outputText))
-            else:
-                outFile.write("<code class='esolf_{0}' tooltip='{2}'>{1}</code>".format(Token.toString(token.type), outputText, tooltip))
-
-            lastToken = token
-
-        outFile.write("</div>\n")    
-        outFile.write("</td>\n")
-        outFile.write("</tr>\n")
-        outFile.write("</tbody>\n")
-        outFile.write("</table>\n")
-        outFile.write("</div>\n")
-        return True
-
-
-    def CopyHtmlResources(self, outputPath):
-        shutil.copyfile("resources/esoluafile.css", os.path.join(outputPath, '') + "esoluafile.css")
-        return True
-
-
-    def CreateHtml(self, outputBasePath, globalData, copyResources = True):
-        outputBasePath = os.path.join(outputBasePath, "").replace("\\", "/")
-        outputFilename = outputBasePath + self.relFilename
-        outputHtmlFilename = outputFilename  + ".html"
-        path = os.path.dirname(outputFilename)
-
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        if copyResources:
-            self.CopyHtmlResources(os.path.dirname(os.path.dirname(outputBasePath)))
-            
-        shutil.copyfile(self.fullFilename, outputFilename)
-
-        templateVars = self.CreateTemplateVars(globalData)
-        templateVars["resourcePath"] = os.path.relpath(outputBasePath, outputFilename).replace("\\", "/")
-
-        with open(outputHtmlFilename, "w") as outFile:
-            outFile.write(self.headerTemplate.safe_substitute(templateVars))
-            self.CreateHtmlContent(outFile, globalData)
-            outFile.write(self.footerTemplate.safe_substitute(templateVars))
-        
-        return True
     
-#
-# filename = app/test/asd.lua
-# outputPath = d:/test/esodata/src/
-# resourcePath = ../../
-#
 
 def LoadFile(filename, relPath = ""):
     newFile = CEsoLuaFile()
@@ -216,11 +67,3 @@ def LoadAllFiles(path, relPath = ""):
     return luaFiles
 
 
-def CreateHtmlAllFiles(esoFiles, outputBasePath, globalData):
-    hasOutputResources = False
-
-    print "Creating HTML versions of {0} Lua files...".format(len(esoFiles))
-
-    for file in esoFiles:
-        file.CreateHtml(outputBasePath, globalData, not hasOutputResources)
-        hasOutputResources = True

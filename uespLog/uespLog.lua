f92e7e2b7797fbb95d60fd7cdd596e27be571252
Dave Humphrey <dave@uesp.net>
2015-08-27 14:49:57 +0100

    change release date

--- uespLog/uespLog.lua
+++ uespLog/uespLog.lua
@@ -228,7 +228,7 @@
 --			- Style of rings and necklaces will no longer be shown.
 --			- Item tooltips for stacks of items in the guild store include a "price per item" line.
 --
---		- v0.30 - 31 Aug 2015
+--		- v0.30 - 30 Aug 2015
 --			- Tweaked /uesptime output, added day of month and adjusted moon phase calculation.
 --			- Online game time can be seen at: http://content3.uesp.net/esoclock/esoclock.php
 --			- Update API version to 1000012.
@@ -246,7 +246,7 @@
 uespLog = { }
 
 uespLog.version = "0.30"
-uespLog.releaseDate = "31 August 2015"
+uespLog.releaseDate = "30 August 2015"
 uespLog.DATA_VERSION = 3
 
 	-- Saved strings cannot exceed 1999 bytes in length (nil is output corrupting the log file)

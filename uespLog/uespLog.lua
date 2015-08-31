23aeada0bd12968c5c833ef8b0e9cfd25e400a18
Dave Humphrey <dave@uesp.net>
2015-08-31 17:44:00 +0100

    add ability icon to skill dumps

--- uespLog/uespLog.lua
+++ uespLog/uespLog.lua
@@ -245,12 +245,15 @@
 --				/uespdump skills 
 --				/uespdump skills [basic/progression/learned/types/all] [note]
 --
+--		- v0.31 -
+--			- Ability icons are logged in skill dumps with the new GetAbilityIcon(abilityId) function.
+--
 
 
 --	GLOBAL DEFINITIONS
 uespLog = { }
 
-uespLog.version = "0.30"
+uespLog.version = "0.31"
 uespLog.releaseDate = "30 August 2015"
 uespLog.DATA_VERSION = 3
 
@@ -3915,6 +3918,7 @@ function uespLog.DumpSkill(abilityId)
 	logData.cost = cost
 	logData.mechanic = mechanic
 	logData.desc = tostring(descHeader) .. tostring(description)
+	logData.icon = GetAbilityIcon(abilityId)
 	
 	if (upgradeLines and upgradeLines ~= "") then logData.upgradeLines = upgradeLines end
 	if (effectLines and effectLines ~= "") then logData.effectLines = effectLines end

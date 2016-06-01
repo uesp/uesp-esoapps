bfc9d9235044048d120dc107cd573d39b9a0e255
Dave Humphrey <dave@uesp.net>
2016-06-01 10:51:02 -0400

    update for DB release

---
+++ parseGlobals/makeApiDiff.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+OLDAPI="100014"
+NEWAPI="100015"
+APIPATH="/cygdrive/d/esoexport/apidata/output"
+
+OLDAPIPATH="$APIPATH/$OLDAPI/data"
+NEWAPIPATH="$APIPATH/$NEWAPI/data"
+
+find "$OLDAPIPATH" -name *.html | sed "s#$OLDAPIPATH.*/#       #g; s#\.html#()#g" | sort > "$APIPATH/files_$OLDAPI.txt"
+find "$NEWAPIPATH" -name *.html | sed "s#$NEWAPIPATH.*/#       #g; s#\.html#()#g" | sort > "$APIPATH/files_$NEWAPI.txt"
+
+comm -23 "$APIPATH/files_$OLDAPI.txt" "$APIPATH/files_$NEWAPI.txt" > "$APIPATH/files_Removed_$OLDAPI.txt"
+comm -13 "$APIPATH/files_$OLDAPI.txt" "$APIPATH/files_$NEWAPI.txt" > "$APIPATH/files_Added_$OLDAPI.txt"
+
+echo -e "Added Functions" > "$APIPATH/apidiff_$NEWAPI.txt"
+grep -v "\." "$APIPATH/files_Added_$OLDAPI.txt" >> "$APIPATH/apidiff_$NEWAPI.txt"
+
+echo -e "\n\nRemoved Functions" >> "$APIPATH/apidiff_$NEWAPI.txt"
+grep -v "\." "$APIPATH/files_Removed_$OLDAPI.txt" >> "$APIPATH/apidiff_$NEWAPI.txt"

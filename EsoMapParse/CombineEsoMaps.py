41681970ac5f3b5105b6777d12dc3df56e2912d7
Dave Humphrey <dave@uesp.net>
2016-10-06 12:28:07 -0400

    update 12 changes

--- EsoMapParse/CombineEsoMaps.py
+++ EsoMapParse/CombineEsoMaps.py
@@ -6,7 +6,7 @@ from PIL import Image
 import shutil
 import fnmatch
 
-BasePathIndex = "-12pts"
+BasePathIndex = "-12"
 InputPath  = 'e:\\esoexport\\esomnf' + BasePathIndex + '\\art\\maps\\'
 OutputPath = 'e:\\esoexport\\goodimages' + BasePathIndex + '\\combinedmaps\\'
 

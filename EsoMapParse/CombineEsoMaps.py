4765fcc10ae2b326c534138c0cafa01403929290
Dave Humphrey <dave@uesp.net>
2017-01-05 16:42:03 -0500

    map update for homestead

--- EsoMapParse/CombineEsoMaps.py
+++ EsoMapParse/CombineEsoMaps.py
@@ -6,7 +6,7 @@ from PIL import Image
 import shutil
 import fnmatch
 
-BasePathIndex = "-12"
+BasePathIndex = "-13pts"
 InputPath  = 'e:\\esoexport\\esomnf' + BasePathIndex + '\\art\\maps\\'
 OutputPath = 'e:\\esoexport\\goodimages' + BasePathIndex + '\\combinedmaps\\'
 

982781a7b8e48693727b7d7d655b11c64fa781e9
Dave Humphrey <dave@uesp.net>
2017-02-07 11:33:50 -0500

    text effect events

--- EsoMapParse/CreateEsoMapTiles.py
+++ EsoMapParse/CreateEsoMapTiles.py
@@ -7,7 +7,7 @@ import math
 import errno
 import csv
 
-BasePathIndex = "-13pts"
+BasePathIndex = "-13"
 INPUTPATH = "e:\\esoexport\\goodimages" + BasePathIndex + "\\combinedmaps\\"
 OUTPUTPATH = "e:\\esoexport\\goodimages" + BasePathIndex + "\\maps\\"
 DEFAULTNULLTILE = "e:\\esoexport\\goodimages" + BasePathIndex + "\\maps\\blacknulltile.jpg"

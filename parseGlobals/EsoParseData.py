29abe7d386059ca0aaf33d242aa2324da4d3b75d
Dave Humphrey <dave@uesp.net>
2017-05-23 16:04:26 -0400

    update 14

--- parseGlobals/EsoParseData.py
+++ parseGlobals/EsoParseData.py
@@ -4,9 +4,9 @@ import EsoFunctionInfo
 import json
 
     # Modify below constants to point to relevant files/paths
-INPUT_GLOBAL_FILENAME = "/cygdrive/e/esoexport/goodimages-13/globals.txt"
-INPUT_LUA_PATH = "/cygdrive/e/esoexport/gamemnf-13/esoui/"
-OUTPUT_PATH = "/cygdrive/e/esoexport/apidata/output/100018/"
+INPUT_GLOBAL_FILENAME = "/cygdrive/e/esoexport/goodimages-14/globals.txt"
+INPUT_LUA_PATH = "/cygdrive/e/esoexport/gamemnf-14/esoui/"
+OUTPUT_PATH = "/cygdrive/e/esoexport/apidata/output/100019/"
 
     # Set to your specific Google CSE ID if you want site search
 GOOGLE_SEARCH_ENGINE_ID = "012503365948233236492:dsuicagnjii"

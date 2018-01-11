f8687062532ebd8582bc6dfc3d4c9190470d7c9b
Dave Humphrey <dave@uesp.net>
2018-01-11 14:44:29 -0500

    fixes for update 17

--- EsoMapParse/CreateEsoMapTiles.py
+++ EsoMapParse/CreateEsoMapTiles.py
@@ -7,7 +7,7 @@ import math
 import errno
 import csv
 
-BasePathIndex = "-16"
+BasePathIndex = "-17pts"
 INPUTPATH = "e:\\esoexport\\goodimages" + BasePathIndex + "\\combinedmaps\\"
 OUTPUTPATH = "e:\\esoexport\\goodimages" + BasePathIndex + "\\maps\\"
 DEFAULTNULLTILE = "e:\\esoexport\\goodimages" + BasePathIndex + "\\maps\\blacknulltile.jpg"
